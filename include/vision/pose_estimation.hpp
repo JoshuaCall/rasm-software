c0de921f0ecc18f6c57a62bf2927496ce4729310
Joshua Call <joshuacall@protonmail.com>
2018-09-30 19:44:06 -0600

    Implementation of MarkerPoseEstimator.

--- include/vision/pose_estimation.hpp
+++ include/vision/pose_estimation.hpp
@@ -121,7 +121,28 @@ protected:
 
 
 public:
+  FacePoseEstimator(CameraImageBuffer &camera, const std::string &circle_detector_filepath,const std::string &pose_model_filepath)
+  : TrackingPoseEstimator(camera)
+  {
+    dlib:::deserialze(circle_detector_filepath) >> circle_detector;
+    dlib::deserialize(pose_model_filepath) >> circle_pose_model;
 
+    std::array<cv::Point3d, 14> temp_pts = {
+      cv::Point3d(-1,-1,0),     
+      cv::Point3d(-1, 1, 0),   
+      cv::Point3d(1, 1, 0),   
+      cv::Point3d(1, -1, 0),
+    }
+    object_pts = std::vector<cv::Point3d>(temp_pts.begin(), temp_pts.end());
+
+    std::array<int, 14> temp_inds = {
+      17,   
+      21,  
+      22, 
+      26,
+    }
+    image_pt_indices = std::vector<int>(temp_inds.begin(), temp_inds.end());
+     
 };
 
 #endif
